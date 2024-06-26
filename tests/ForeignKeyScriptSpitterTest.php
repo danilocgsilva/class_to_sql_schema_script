c9773c4dc9d8f17fe5e9da5495e0dd428189b152
Danilo Silva <contact@danilocgsilva.me>
2024-06-26 17:50:06 +0000

    wip: adding test with foreign key

--- tests/ForeignKeyScriptSpitterTest.php
+++ tests/ForeignKeyScriptSpitterTest.php
@@ -24,5 +24,32 @@ class ForeignKeyScriptSpitterTest extends TestCase
 
         $this->assertSame($expectedString, $foreignKeyScriptSpitter->getScript());
     }
+
+    public function testScriptWithDatabaseScript(): void
+    {
+        $expectedString = <<<EOF
+CREATE DATABASE IF NOT EXISTS fieldsman_test;
+USE fieldsman_test;
+
+CREATE TABLE IF NOT EXISTS payloads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    content TEXT
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS field_payload (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    field_id INT UNSIGNED NOT NULL,
+    payload_id INT UNSIGNED NOT NULL
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE field_payload ADD CONSTRAINT \`field_payload_payload_constaint\` FOREIGN KEY (\`payload_id\`) REFERENCES payloads (\`id\`);  
+EOF;
+
+        $databaseScriptSpitter = new DatabaseScriptSpitter("fieldsman_test");
+        $databaseScriptSpitter->setUseSelf();
+
+        $tablePayloadsSpitter = new TableScriptSpitterPayload();
+    }
 }
 
